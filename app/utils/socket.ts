3d31c4791ca0b60977bddf1733ce75a46418e7e8
esc4n0rx <paulo.cunha@hortifruti.com.br>
2024-12-05 11:16:43 -0300

    Atualizando url backend

--- app/utils/socket.ts
+++ app/utils/socket.ts
@@ -3,7 +3,7 @@ let socket: WebSocket | null = null;
 export const getSocket = (roomId: string): WebSocket => {
   if (!socket || socket.readyState === WebSocket.CLOSED) {
     const wsProtocol = window.location.protocol === 'https:' ? 'wss' : 'ws';
-    socket = new WebSocket(`${wsProtocol}://shadow-backend-production.up.railway.app/ws/${roomId}`);
+    socket = new WebSocket(`${wsProtocol}://shadow-backend-88tg.onrender.com/ws/${roomId}`);
   }
   return socket;
 };
